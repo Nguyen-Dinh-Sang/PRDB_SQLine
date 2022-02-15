9e3d382a9da17c480f9a59b0e6c86a0d1682bc5e
Nguyen-XuanLinh <xuanlinh99.24@gmail.com>
2022-02-15 23:19:59 +0700

    NewTriple-SangND: create new triple, modified query execution and select condition

---
+++ PRDB_Sqlite/BLL/ProbValueOfTriple.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRDB_Sqlite.BLL
+{
+    public class ValueOfTriple
+    {
+        public List<Object> Value { get; set; }
+
+        public ValueOfTriple()
+        {
+            this.Value = new List<object>();
+        }
+
+        public ValueOfTriple(String multiValue)
+        {
+            this.Value = new List<object>();
+            if (multiValue.Contains(','))
+            {
+                string[] singleValues = multiValue.Split(',');
+                for (int i = 0; i < singleValues.Length; i++)
+                {
+                    this.Value.Add(singleValues[i]);
+                }
+            } else
+            {
+                this.Value.Add(multiValue);
+            }
+        }
+
+        public string GetStrValue()
+        {
+            string strValue = "";
+
+            for (int i = 0; i < Value.Count; i++)
+            {
+                strValue += Value[i].ToString().Trim();
+                strValue += ", ";
+            }
+            if (strValue != "")
+                strValue = strValue.Remove(strValue.Length - 2); // loại bỏ kí tự ', ' thừa
+
+            return strValue;
+        }
+    }
+}
